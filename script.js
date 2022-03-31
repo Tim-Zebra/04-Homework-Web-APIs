3964de0bf4c0935337e69872a77e41bded674f6f
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-03-31 16:14:23 -0500

    added some pseudo code to js and html

---
+++ script.js
@@ -0,0 +1,35 @@
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ A button for view Highscores  //////////////////////////////////////////
+// displays in top left corner. When highscores are being view, dispears
+
+
+
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ A timer that counts down  //////////////////////////////////////////////
+// displays in top right corner.
+// Appears only during quiz
+// counts down from x
+
+
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Main Content: Questions, /////////////////////////////////////////////////////
+
+// function that changes the header (this function will also remove the paragraph at the start)
+
+// Display a question with answers
+
+
+// Output a question and 4 possible answers
+// Create an ordered list for the answers
+
+// Be able to select an Answer
+
+// Determine if answer is correct or not, and consequences
+
+// Event listener for selecting buttons
+
+
+
+
+
+
+
