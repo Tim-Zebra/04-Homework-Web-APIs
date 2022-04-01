ff59849775e8f02219b5ab89390fa97366fee854
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-03-31 20:38:22 -0500

    Question Bank tested and stores as intended. getQuestion function created to retreive values from Question Bank, and functions as intended. ToDo: increase scope of variables to be sent back and displayed to HTML.

--- script.js
+++ script.js
@@ -16,42 +16,75 @@ function timer() {
     // Initial clock in seconds.
     var timeLeft = 55;
     timerEl.textContent = "Time Remaining: " + timeLeft;
-    
+
+    // Time counter
     var timeInterval = setInterval(function () {
       timeLeft--;
       timerEl.textContent = "Time Remaining: " + timeLeft;
     
-      // Stops execution of action at set interval
       if(timeLeft === 0) {
-
         clearInterval(timeInterval);
         timerEl.textContent = "TIMES UP!";
-        // Calls function to create and append image
-        // displayMessage();
       }
      }, 1000);
   }
 
-timer();
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Main Content: Questions, /////////////////////////////////////////////////////
+
+//varibles needed for main quiz function
+var mainHeader = document.getElementById('mainHeader');
 
 // function that changes the header (this function will also remove the paragraph at the start)
 
-// Display a question with answers
+//pull a question from the question bank
 
+// Display a question with possible answers
 
-// Output a question and 4 possible answers
-// Create an ordered list for the answers
+// Take in the user selection
 
 // Be able to select an Answer
 
 // Determine if answer is correct or not, and consequences
 
-// Event listener for selecting buttons
 
 
+// \\\\\\\\\\\\\\\\\\\\\\\QUESTION BANK///////////////////////////////
 
+// Stores questions
+var questionBank = {
+    question1: {
+        question: 'This is a Question#1?',
+        answers: {
+            choice1: ["This is a answer #1?", false],
+            choice2: ["This is a answer #2?", false],
+            choice3: ["This is a answer #3?", true],
+            choice4: ["This is a answer #4?", false],
+        }
+    },
+    question2: {
+        question: 'This is a Question#2?',
+        answers: {
+            choice1: ["This is a answer #1?", false],
+            choice2: ["This is a answer #2?", false],
+            choice3: ["This is a answer #3?", true],
+            choice4: ["This is a answer #4?", false],
+        }
+    }
+}
 
+// variable with the requested question#
+var questionNumber = 1;
 
+// Retrieves the question and puts value to the question and answer variables
+// To use: answer is an array. answer[0]=the text for the answer, answer[1]=if the answer is correct or not
+function getQuestion(questionNumber) {
+    var questionChooser = 'question'+questionNumber;
+    // gets all variables of the question: Question, Answer, Correct/Wrong
+    var question = questionBank[questionChooser].question;
+    var answer1 = questionBank[questionChooser].answers.choice1;
+    var answer2 = questionBank[questionChooser].answers.choice2;
+    var answer3 = questionBank[questionChooser].answers.choice3;
+    var answer4 = questionBank[questionChooser].answers.choice4;
 
-
+    // TODO; need to attach to global elements that link to the quetion
+}
