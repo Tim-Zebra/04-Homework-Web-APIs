e6303d5c89578d54bd89850a2a4cf17c3ea43dae
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-03-31 20:02:19 -0500

    added timer function in JS. Added some more structure to HTML.

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ A button for view Highscores  //////////////////////////////////////////
 // displays in top left corner. When highscores are being view, dispears
-
+var highschoresEl = document.getElementById('highscores');
 
 
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ A timer that counts down  //////////////////////////////////////////////
 // displays in top right corner.
 // Appears only during quiz
-// counts down from x
 
+//variables needed for timer function
+var timerEl = document.getElementById('timer');
 
+// Timer that counts down by seconds
+function timer() {
+    // Initial clock in seconds.
+    var timeLeft = 55;
+    timerEl.textContent = "Time Remaining: " + timeLeft;
+    
+    var timeInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = "Time Remaining: " + timeLeft;
+    
+      // Stops execution of action at set interval
+      if(timeLeft === 0) {
 
+        clearInterval(timeInterval);
+        timerEl.textContent = "TIMES UP!";
+        // Calls function to create and append image
+        // displayMessage();
+      }
+     }, 1000);
+  }
+
+timer();
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Main Content: Questions, /////////////////////////////////////////////////////
 
 // function that changes the header (this function will also remove the paragraph at the start)
