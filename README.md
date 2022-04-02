8ea6f938078de3062974105614ad39fa8b88f6ff
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-01 21:31:48 -0500

    Added more pseudocode to JS. Created and tested init function (works). Created and tested startQuiz + event listener (works), still need to add more actions. Added html elements for initial start of screen.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 04-Homework-Web-APIs
 UT Bootcamp - A timed "code concepts" multiple choice quiz built  from scratch using web APIs and JS
+
+JS - function init() inspired by UT Bootcamp Course Web-APIs
--- script.js
+++ script.js
@@ -1,11 +1,62 @@
+// intializes the program, pulls highscores from storage.
+function init() {
+    // Loads highscores from local dat
+    var storedHighscores = JSON.parse(localStorage.getItem("Highscores"))
+
+    // Determines if there was anything in local storage. And if not, then do nothing.
+    if (storedHighscores !== null) {
+        highscores = storedHighscores;
+    }
+
+}
+
+init();
+
+// Starts the quiz
+var startQuiz = document.querySelector(".startQuiz")
+
+startQuiz.addEventListener("click", function() {
+    //Remove the <p> element describing the game
+    var introParagraph = document.getElementById("introParagraph");
+    if (introParagraph !== null){
+        introParagraph.remove();
+    }
+    // Starts the timer
+    timer();
+    // Hide Start Button
+    startQuiz.remove();
+    // Change the header to "Question:1"
+    // Display the answers 
+    // Display the submit button
+
+});
+
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ A button for view Highscores  //////////////////////////////////////////
 // displays in top left corner. When highscores are being view, dispears
+var highscores = {};
 var highschoresEl = document.getElementById('highscores');
 
+// // Submit highscore 
+// function submitHighscore () {
+//     event.preventDefault();
+// }
 
+// // Store highscore locally
+// function storeHighscore () {
+
+// }
+
+// // display highscore list
+// function displayHighscore () {
+
+// }
+
+// When highscores button is selected, before game starts, player can view highscores
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ A timer that counts down  //////////////////////////////////////////////
-// displays in top right corner.
+// displays Timer
+
+// Timer displays in top right corner.
 // Appears only during quiz
 
 //variables needed for timer function
@@ -14,7 +65,7 @@ var timerEl = document.getElementById('timer');
 // Timer that counts down by seconds
 function timer() {
     // Initial clock in seconds.
-    var timeLeft = 55;
+    var timeLeft = 20;
     timerEl.textContent = "Time Remaining: " + timeLeft;
 
     // Time counter
@@ -25,11 +76,18 @@ function timer() {
       if(timeLeft === 0) {
         clearInterval(timeInterval);
         timerEl.textContent = "TIMES UP!";
+        gameEnd();
       }
      }, 1000);
   }
 
 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Main Content: Questions, /////////////////////////////////////////////////////
+// Ends the game. displays 
+function endGame () {
+    // Removes the option to question and answer
+    // Displays the form to submit highscore
+}
+
 
 //varibles needed for main quiz function
 var mainHeader = document.getElementById('mainHeader');
